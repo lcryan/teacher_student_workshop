6b531483633df4573f1635e6a7eb9839e78050b1
LuciaRyan <lcryan06@gmail.com>
2023-09-12 23:04:57 +0200

    started teacherController - incomplete

--- src/main/java/com/example/workshopdtosbackend/controllers/TeacherController.java
+++ src/main/java/com/example/workshopdtosbackend/controllers/TeacherController.java
@@ -1,4 +1,17 @@
 package com.example.workshopdtosbackend.controllers;
 
+
+import com.example.workshopdtosbackend.service.TeacherService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teachers")
 public class TeacherController {
+
+    private final TeacherService teacherService;
+
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
 }
