d973881075b9f711d42f008bfb6c252941675d2c
LuciaRyan <lcryan06@gmail.com>
2023-09-12 22:46:56 +0200

    basic set up for repos

--- src/main/java/com/example/workshopdtosbackend/repositories/StudentRepository.java
+++ src/main/java/com/example/workshopdtosbackend/repositories/StudentRepository.java
@@ -1,4 +1,7 @@
 package com.example.workshopdtosbackend.repositories;
 
-public interface StudentRepository {
+import com.example.workshopdtosbackend.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
