42c75465ec8a7805641e0e6b58903fd7716d7db5
LuciaRyan <lcryan06@gmail.com>
2023-09-12 23:01:32 +0200

    studentDto completed including validation ann.

--- src/main/java/com/example/workshopdtosbackend/dtos/StudentDto.java
+++ src/main/java/com/example/workshopdtosbackend/dtos/StudentDto.java
@@ -1,4 +1,19 @@
 package com.example.workshopdtosbackend.dtos;
 
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
 public class StudentDto {
+    public Long id;
+    @NotBlank(message = "id missing")
+    public String fullName;
+    @Size(min = 7, max = 100)
+    public String mainSubject;
+
+    public LocalDate startingDate;
+
+    @Min(500)
+    @Max(5000)
+    public int studentLoan;
 }
