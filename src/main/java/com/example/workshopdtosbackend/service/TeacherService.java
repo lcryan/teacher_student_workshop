0433ae8a5eb86f460023e8591db946f307e2739d
LuciaRyan <lcryan06@gmail.com>
2023-09-12 23:05:43 +0200

    started coding teacherService - still incomplete

--- src/main/java/com/example/workshopdtosbackend/service/TeacherService.java
+++ src/main/java/com/example/workshopdtosbackend/service/TeacherService.java
@@ -1,4 +1,15 @@
 package com.example.workshopdtosbackend.service;
 
+
+import com.example.workshopdtosbackend.repositories.TeacherRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TeacherService {
+
+    private final TeacherRepository teacherRepository;
+
+    public TeacherService(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
 }
